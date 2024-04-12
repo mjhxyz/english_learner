29bf784ad44f37c3967a9f1b468e23401f59e90c
mjhxyz <mjhxyz@foxmail.com>
2024-04-12 16:57:06 +0800

    优化首页查询

--- app/view/words.py
+++ app/view/words.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 words_bp = Blueprint('words', __name__)
@@ -7,4 +7,5 @@ words_bp = Blueprint('words', __name__)
 @words_bp.route('/', methods=['GET'])
 @login_required
 def index():
-    return render_template('words.j2')
+    args = request.args
+    return render_template('words.j2', kw=args.get('kw', ''))
